69178b1e7725d6e1b6f814c5e4c8b055fb80765d
kruchkamgar <kruchkamgar@gmail.com>
2018-09-22 13:54:52 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    Post.create(title: "Kurosh's Test Post", description: "Post description")
